6c15707bfc3ff6ca532274939a5b8aee9680b536
z5211269 <36873768+z5211269@users.noreply.github.com>
2019-08-13 15:28:26 +1000

    Update views.py

--- mysite/example/views.py
+++ mysite/example/views.py
@@ -20,12 +20,28 @@ def example_get(request, var_a, var_b):
 		return JsonResponse({"isError": True, "error":str(e), "errorType":errorType, "function":fname, "line":exc_tb.tb_lineno, "log":log})
 
 @csrf_exempt
-def example_post(request):
-	log = []
+ def example_post(request):
+	jsob={"startnumber":0, "length":10}
+	log=[]
 	if request.method == "POST":
 		try:
 			data = request.POST["data"]
-			return JsonResponse({"log":log})
+			received = json.loads(data)
+			jsob.update(received)
+
+			startnumber = int(jsob["startnumber"])
+			length = int(jsob["length"])
+
+			loop = range(length)
+			numarray = []
+			fibno = startnumber
+			addno = 1
+			for l in loop:
+				numarray.append(fibno)
+				fibno = fibno + addno
+				addno = fibno - addno
+			return JsonResponse({"fib number":numarray})
+		
 		except Exception as e:
 			exc_type, exc_obj, exc_tb = sys.exc_info()
 			other = sys.exc_info()[0].__name__
@@ -34,8 +50,7 @@ def example_post(request):
 			return JsonResponse({"isError": True, "error":str(e), "errorType":errorType, "function":fname, "line":exc_tb.tb_lineno, "log":log})
 	else:
 		return HttpResponse("<h1>ONLY POST REQUESTS</h1>")
-	
-	
+
 @csrf_exempt
 def Fibonacci_series(Number):
 nterms = int(10)
@@ -56,7 +71,8 @@ else:
        n1 = n2
        n2 = nth
        count += 1
-except Exception as e:
+	   
+		except Exception as e:
 			exc_type, exc_obj, exc_tb = sys.exc_info()
 			other = sys.exc_info()[0].__name__
 			fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
