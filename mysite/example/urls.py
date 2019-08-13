0a38f25cdd5cfa9496b713e61e77c608f00a65ad
z5211269 <36873768+z5211269@users.noreply.github.com>
2019-08-14 02:18:19 +1000

    Update urls.py

--- mysite/example/urls.py
+++ mysite/example/urls.py
@@ -5,6 +5,4 @@ from . import views
 urlpatterns = [
 	path(r'example_get/<str:var_a>/<int:var_b>',  views.example_get),
 	path(r'example_post/', views.example_post),
-	path(r'Fibonacci_series/', views.Fibonacci_series),
-	Fibonacci_series
 ]
